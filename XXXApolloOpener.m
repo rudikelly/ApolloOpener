abb0ed48497605ea92169b33ab8badd9a0cbe27e
Rudi Kelly <rudikellyy@gmail.com>
2018-07-26 12:49:54 +0100

    Fixed issue regarding the replacement of 'http' with 'apollo'

--- XXXApolloOpener.m
+++ XXXApolloOpener.m
@@ -20,12 +20,8 @@
 - (id)openURL:(NSURL *)url sender:(NSString *)sender {
 	NSString *urlString = [url.absoluteString lowercaseString];
     if ([url.host isEqualToString:@"www.m.reddit.com"] || [url.host isEqualToString:@"www.reddit.com"] || [url.host isEqualToString:@"m.reddit.com"] || [url.host isEqualToString:@"reddit.com"] || [url.host containsString:@".reddit.com"]) {
-		if([urlString containsString:@"https"]){
-			urlString = [urlString stringByReplacingOccurrencesOfString:@"https" withString:@"apollo"];
-		}else if([urlString containsString:@"http"]){
-			urlString = [urlString stringByReplacingOccurrencesOfString:@"http" withString:@"apollo"];
-		}
-		
+      urlString.scheme = @"apollo";
+
 		if([urlString isEqualToString:@"apollo://www.reddit.com/"] || [urlString isEqualToString:@"apollo://reddit.com/"] || [urlString isEqualToString:@"apollo://www.reddit.com"] || [urlString isEqualToString:@"apollo://reddit.com"]){
 			urlString = @"apollo://";
 		}
